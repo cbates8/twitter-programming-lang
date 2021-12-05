6e2ac36ea68d924791f2296b5dd31fdd4d5d2de0
Luke Manzitto <lukemanzitto@gmail.com>
2021-12-05 04:54:16 -0800

    added language dataset and import functionality

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from searchtweets import ResultStream, gen_request_parameters, load_credentials
+import json
+
+# currently will print all programming languages from dataset
+def GenQuery():
+    file = open("All_Programming_Languages.json","r")
+    info = file.read()
+    languages = json.loads(info)
+    for language in languages["results"]:
+        print(language["ProgrammingLanguage"])
+
 def main():
     search_args = load_credentials(".twitter_keys.yaml",
                                         yaml_key="search_tweets_v2",
                                         env_overwrite=False)
 
-    query = gen_request_parameters("snow", granularity=False, results_per_call=10)
+    query = gen_request_parameters("(favorite programming language) -is:retweet", granularity=False, results_per_call=10)
     print(query)
 
     rs = ResultStream(request_parameters=query,
@@ -19,4 +29,4 @@ def main():
     [print(tweet) for tweet in tweets[0:10]]
 
 if __name__ == "__main__":
-    main()
+    GenQuery()
