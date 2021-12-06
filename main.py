33fc921b3affcac3169f6f5906771f448fb81ea7
Luke Manzitto <lukemanzitto@gmail.com>
2021-12-05 20:17:08 -0800

    oof

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from searchtweets import ResultStream, gen_request_parameters, load_credentials
+from nltk.sentiment import SentimentIntensityAnalyzer
 import json, nltk
+import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
 from wordcloud import WordCloud
 import re
 
+
 # currently will print all programming languages from dataset
-def GenQuery():
+def GetLanguages():
     file = open("All_Programming_Languages.json","r")
     info = file.read()
+    file.close()
     languages = json.loads(info)
     return [lang["ProgrammingLanguage"] for lang in languages["results"]]
 
@@ -42,9 +47,47 @@ def main():
             wrds = t["text"].replace("“", "")
             wrds = wrds.replace("?", "")
             wrds = re.split('\n |\ |, |\*|\n', wrds)
+            words = [w for w in wrds]
+            tweets[t["id"]] = " ".join(words)
+            for lang in GetLanguages():
+                if lang in wrds or lang.lower() in wrds:
+                    all_languages += f" {lang} "
+                    if lang not in languages.keys():
+                        languages[lang] = " ".join(words)
+                    else:
+                        languages[lang] += f" {' '.join(words)} "
+    sia = SentimentIntensityAnalyzer()
+    print("Generating Sentiment Polarity Scores for languages...")
+    sia_scores = {}
+    for key,value in languages.items():
+        sia_scores[key] = sia.polarity_scores(value)
+    xValues = []
+    yValues = []
+    zValues = []
+    for lang in languages.values():
+        if lang in sia_scores.keys():
+            xValues.append(sia_scores[lang]["neg"])
+            yValues.append(sia_scores[lang]["neu"])
+            zValues.append(sia_scores[lang]["pos"])
+    x = np.array(xValues)
+    y = np.array(yValues)
+    z = np.array(zValues)
+
+    plt.figure(figsize=(8,8))
+    ax = plt.axes(projection="3d")
+    ax.scatter(x,y,z)
+    plt.savefig(f"./polarity_scores.png")
+    print(f"Saved as 'polarity_scores.png'\n")
+
+    for data in all_tweets[0:10]:
+        for tweet in list(dict(data)["data"]):
+            t = dict(tweet)
+            wrds = t["text"].replace("“", "")
+            wrds = wrds.replace("?", "")
+            wrds = re.split('\n |\ |, |\*|\n', wrds)
             words = [w for w in wrds if w.lower() not in stopwords and "http" not in w]
             tweets[t["id"]] = " ".join(words)
-            for lang in GenQuery():
+            for lang in GetLanguages():
                 if lang in wrds or lang.lower() in wrds:
                     all_languages += f" {lang} "
                     if lang not in languages.keys():
@@ -76,7 +119,7 @@ def main():
 
     # WordCloud for specific languages
     print("Generating wordcloud for individual languages...")
-    for lang in GenQuery():
+    for lang in GetLanguages():
         if lang in languages.keys():
             words = languages[lang].replace("C++", "Cpp")
             wordcloud.generate(words)
